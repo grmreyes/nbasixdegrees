479aba95850068699c5cc2cf496f49e8cd7052d3
Gino Reyes <grmreyes@gmail.com>
2021-05-22 00:20:58 +0800

    fix ha, el, yi

--- nbasearch.js
+++ nbasearch.js
@@ -4,7 +4,7 @@ function toTitleCase(str) {
       /\w*/g,
       function(txt) {
         if(txt.length<3){
-            if(!["JA","MO","AL","BO","KY","SI","TY","DE","SR"].includes(txt.toUpperCase())){
+            if(!["JA","MO","AL","BO","KY","SI","TY","DE","SR","EL","HA","YI"].includes(txt.toUpperCase())){
                 // console.log(txt.toUpperCase())
                 return txt.toUpperCase();
             }
